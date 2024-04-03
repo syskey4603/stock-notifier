a8ecbe843f7500e4961718df700d3de6d3808ea3
sxskey <83065600+syskey4603@users.noreply.github.com>
2024-04-03 22:28:52 +0530

    Update stockprice.py

--- stockprice.py
+++ stockprice.py
@@ -1,6 +1,9 @@
 import time
 import yfinance as yf
 import warnings
+from plyer import notification
+
+
 averageprice = 0
 warnings.filterwarnings('ignore', category=FutureWarning)
 
@@ -12,4 +15,11 @@ while True:
         averageprice += price
     averageprice += price/10
     print(f"the average price of reliance in the past 10 days is " + str(averageprice))
+    currentprice = reliance_stock.history(period="1d")["Close"][0]
+    if(currentprice < 2930):
+        notification.notify(
+        title = "Reliance price",
+        message = "Reliance price is currently " + str(currentprice),
+        timeout = 10
+        )
     time.sleep(300)
